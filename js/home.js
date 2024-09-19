9c7b6037a4387d18fc5b2b72490b9c0ab78b168c
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-19 12:31:24 +0600

    add money button done

--- js/home.js
+++ js/home.js
@@ -3,6 +3,23 @@ document.getElementById('money-el').addEventListener('click', function () {
   addMoney.setAttribute('class', 'block');
 });
 
-document.getElementById('logout-btn').addEventListener('click',function(){
-    window.location.href = '../index.html'
-})
+document.getElementById('logout-btn').addEventListener('click', function () {
+  window.location.href = '../index.html';
+});
+
+document
+  .getElementById('addMoney-btn')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    const amountNumber = document.getElementById('amount').value;
+    const pin = document.getElementById('pin-number').value;
+    if (pin === '544143') {
+      let balance = document.getElementById('balance').innerText;
+      const newBalance = parseInt(balance) + parseInt(amountNumber);
+      const mainBalance = document.getElementById('balance');
+      mainBalance.innerText = newBalance;
+    } else {
+      const alertMessage = document.getElementById('pin-alert')
+      alertMessage.innerText = 'আমার ভুল হচে ক্ষমা করে দেন'
+    }
+  });
